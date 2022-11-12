5482f20042ba3481ceacd220fe26b2f791d6932b
Paty Alves <patriciaalves10@gmail.com>
2022-11-11 22:30:30 -0300

    update

--- server.js
+++ server.js
@@ -3,11 +3,28 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/tempo-agora'));
 
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/tempo-agora/index.html'));
 });
 
